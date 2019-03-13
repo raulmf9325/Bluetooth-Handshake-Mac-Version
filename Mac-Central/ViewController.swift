427fe9e8c45884c3753be924e694b82729456c47
raulmf9325 <raulmf9325@ufl.edu>
2019-03-13 13:55:15 -0400

    1st build

--- Mac-Central/ViewController.swift
+++ Mac-Central/ViewController.swift
@@ -9,19 +9,52 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var manager: BluetoothManager?
+    
+    var registeredDevices = [UUID]()
 
+    @IBOutlet weak var numberOfPeripherals: NSTextField!
+   
+    @IBOutlet weak var textView: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        manager = BluetoothManager()
+        manager?.delegate = self
+        numberOfPeripherals.isEditable = false
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
+    
+}
 
-
+extension ViewController: PeripheralUpdateDelegate{
+    
+    func handleCharacteristicUpdate(identifier: UUID) {
+        
+        if !registeredDevices.contains(identifier){
+            
+            registeredDevices.append(identifier)
+            
+            let integer = numberOfPeripherals.stringValue
+            guard var number = Int(integer) else {return}
+            number += 1
+            let text = String(number)
+            
+            numberOfPeripherals.stringValue = text
+            
+            var uuids = textView.stringValue
+            uuids.append("\n")
+            uuids.append(identifier.uuidString)
+            textView.stringValue = uuids
+        }
+        
+    }
 }
 
